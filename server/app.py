26d0abe48cee4b4b685294d27b3dc310edc25f75
ahmed <ahmednoorzakaria680@gmail.com>
2023-10-02 12:19:46 +0300

    cookies and session

--- server/app.py
+++ server/app.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
-
 from models import db, Article, User
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 
@@ -22,13 +18,30 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    article_list = [{'id': article.id, 'title': article.title} for article in articles]
+    return jsonify({'articles': article_list}), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views', 0) + 1
 
-    pass
+    if session['page_views'] > 3:
+        return {'message': 'Maximum pageview limit reached'}, 401
+
+    article = Article.query.get(id)
+
+    if article is None:
+        return {'message': 'Article not found'}, 404
+
+    return jsonify({
+        'article': {
+            'id': article.id,
+            'title': article.title,
+            'content': article.content
+        },
+        'page_views': session['page_views']
+    }), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
